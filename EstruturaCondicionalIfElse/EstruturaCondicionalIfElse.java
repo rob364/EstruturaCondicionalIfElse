bd148b67e25b902d2b339fc1c19c555ffd147d5e
Roberta <vcindustria@gmail.com>
2022-05-13 13:50:07 -0300

    Projeto Estrutura Condicional IfElse

---
+++ EstruturaCondicionalIfElse/EstruturaCondicionalIfElse.java
@@ -0,0 +1,20 @@
+
+public class EstruturaCondicionalIfElse {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		
+		System.out.println("Bom dia!");
+		
+		if (x > 0) { // Estrutura condicional Simples quando é verdadeiro ele executa o bloco de codigo, quando é Falso ele pula o bloco de comando
+			System.out.println("Boa tarde!");
+			
+		}
+		
+		System.out.println("Boa noite!");
+
+
+	}
+
+}
